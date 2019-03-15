b40e3a4e47887a3ac5cf596e1f34fbff409c6828
Hugo Ledoux <h.ledoux@tudelft.nl>
2019-03-15 08:44:09 +0100

    Add code for Vector and simple 2D geometry ops that are needed
    also contains how to make Rust tests, it's nice and oh so welcome

--- src/predicates.rs
+++ src/predicates.rs
@@ -1,4 +1,5 @@
 use super::Point3d;
+use std::ops::{Add, Sub};
 
 pub fn orient2d(a: &Point3d, b: &Point3d, c: &Point3d) -> i8 {
     //-- CCW    = +1
@@ -44,5 +45,80 @@ pub fn incircle(a: &Point3d, b: &Point3d, c: &Point3d, p: &Point3d) -> i8 {
         return 1;
     } else {
         return -1;
+    }
+}
+
+pub struct Vector {
+    pub x: f64,
+    pub y: f64,
+}
+impl Add for Vector {
+    type Output = Vector;
+
+    fn add(self, other: Vector) -> Vector {
+        Vector {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+impl Sub for Vector {
+    type Output = Vector;
+
+    fn sub(self, other: Vector) -> Vector {
+        Vector {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        }
+    }
+}
+impl Vector {
+    pub fn new(ax: f64, ay: f64, bx: f64, by: f64) -> Vector {
+        Vector {
+            x: bx - ax,
+            y: by - ay,
+        }
+    }
+    pub fn dot(&self, other: Self) -> f64 {
+        self.x * other.x + self.y * other.y
+    }
+    pub fn length(&self) -> f64 {
+        (self.x * self.x + self.y * self.y).sqrt()
+    }
+    pub fn normalise(&mut self) {
+        let l = self.length();
+        if l == 0.0 {
+            panic!("Vector of length=0 can't be normalised.");
+        }
+        self.x = self.x / l;
+        self.y = self.y / l;
+    }
+}
+
+pub fn vector_projection(a: &Point3d, b: &Point3d, c: &Point3d) -> f32 {
+    let mut p = Vector::new(a.x, a.y, b.x, b.y);
+    let mut q = Vector::new(a.x, a.y, c.x, c.y);
+
+    p.normalise();
+
+    0.22
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn normalise() {
+        let mut v = Vector::new(0.0, 0.0, 1.0, 0.0);
+        assert_eq!(v.length(), 1.0);
+        v = Vector::new(1.0, 1.0, 1.0, 5.0);
+        assert_eq!(v.length(), 4.0);
+    }
+    #[test]
+    #[should_panic]
+    fn normalise_panic() {
+        let mut v = Vector::new(1.0, 1.0, 1.0, 1.0);
+        assert_eq!(v.length(), 1.0);
     }
 }
