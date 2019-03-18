808a406306949fd0e8321f4f6038e6477f53a657
Hugo Ledoux <h.ledoux@tudelft.nl>
2019-03-18 11:51:18 +0100

    WIP for main.rs

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use std::io;
 // $ ./rustin < ../../data/samples2.xyz
 
 #[derive(Debug, Deserialize)]
-pub struct Point {
+pub struct CSVPoint {
     pub x: f64,
     pub y: f64,
     pub z: f64,
@@ -28,6 +28,7 @@ fn main() {
     let mut tr = rustin::Triangulation::new();
     // tr.set_snap_tolerance(0.1);
     tr.set_jump_and_walk(false);
+
     let mut duplicates = 0;
     for p in vec.into_iter() {
         let re = tr.insert_one_pt(p.x, p.y, p.z);
@@ -56,6 +57,21 @@ fn main() {
     // let ch = tr.get_convex_hull();
     // println!("{:?}", ch);
 
+    let pts = tr.get_vertices();
+    println!("Size pts: {}", pts.len());
+
+    //-- fetch triangle containing (x, y)
+    let re = tr.locate(450.0, 50.0);
+    if re.is_some() {
+        let t = re.unwrap();
+        println!("{}", t);
+        println!("({})", tr.get_point(t.tr0));
+        println!("({})", tr.get_point(t.tr1));
+        println!("({})", tr.get_point(t.tr2));
+    } else {
+        println!("Outside convex hull");
+    }
+
     // let stats = tr.stats_degree();
     // println!("stats: {}--{}--{}", stats.0, stats.1, stats.2);
 
@@ -63,13 +79,13 @@ fn main() {
     //     .unwrap();
 }
 
-fn read_xyz_file() -> Result<Vec<Point>, Box<Error>> {
+fn read_xyz_file() -> Result<Vec<CSVPoint>, Box<Error>> {
     let mut rdr = csv::ReaderBuilder::new()
         .delimiter(b' ')
         .from_reader(io::stdin());
-    let mut vpts: Vec<Point> = Vec::new();
+    let mut vpts: Vec<CSVPoint> = Vec::new();
     for result in rdr.deserialize() {
-        let record: Point = result?;
+        let record: CSVPoint = result?;
         vpts.push(record);
     }
     Ok(vpts)
