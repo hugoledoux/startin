b8dc4d3bf5ee8fe6a5cbe3894879567b4769777a
Hugo Ledoux <h.ledoux@tudelft.nl>
2019-06-03 10:47:59 +0200

    WIP example3.rs

--- examples/example3.rs
+++ examples/example3.rs
@@ -4,28 +4,40 @@ fn main() {
     let mut pts: Vec<Vec<f64>> = Vec::new();
     pts.push(vec![2.0, 2.0, 2.0]);
     pts.push(vec![4.0, 2.0, 12.5]);
-    pts.push(vec![4.0, 5.0, 7.65]);
-    pts.push(vec![2.0, 4.0, 21.0]);
+    pts.push(vec![5.0, 5.0, 7.65]);
     pts.push(vec![2.5, 2.5, 33.0]);
-    pts.push(vec![3.0, 3.1, 33.0]);
+    pts.push(vec![2.0, 4.0, 21.0]);
+    // pts.push(vec![3.0, 3.1, 33.0]);
 
     let mut dt = startin::Triangulation::new();
     dt.insert(&pts);
     println!("{}", dt.printme());
 
     // println!("{}", dt);
-    dt.remove(5);
+    let re = dt.remove(4);
+    if re.is_err() == true {
+        println!("!!!Error: {:?}", re.unwrap_err());
+    }
     println!("{}", dt.printme());
+
+    let re = dt.insert_one_pt(2.3, 2.3, 2.0);
+    let re = dt.insert_one_pt(2.6, 2.6, 2.0);
+    let re = dt.insert_one_pt(2.9, 2.6, 2.0);
+    let re = dt.insert_one_pt(2.9, 2.1, 2.0);
+    println!("{}", dt.printme());
+    // println!("{}", dt.printme());
+
     // println!("{}", dt);
     dt.remove(6);
     println!("{}", dt.printme());
 
-    let re = dt.insert_one_pt(3.3, 3.2, 2.0);
-    let re = dt.insert_one_pt(2.3, 2.2, 22.0);
-    println!("{}", dt.printme());
-
+    // let re = dt.insert_one_pt(3.3, 3.2, 2.0);
+    // let re = dt.insert_one_pt(2.3, 2.2, 22.0);
+    // println!("{}", dt.printme());
 
     println!("Is Delaunay?: {}", dt.is_valid());
+    println!("# vertices {}", dt.number_of_vertices());
+    println!("# triangles {}", dt.number_of_triangles());
 
     // let re = dt.insert_one_pt(20.0, 30.0, 2.0);
     // match re {
