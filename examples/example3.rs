0d608c21a4ce7996e8b99efd4146e707e79e084c
Hugo Ledoux <h.ledoux@tudelft.nl>
2019-06-03 14:51:04 +0200

    Clean example3.rs to debug remove()

--- examples/example3.rs
+++ examples/example3.rs
@@ -4,21 +4,14 @@ use rand::prelude::*;
 fn main() {
     let mut pts: Vec<Vec<f64>> = Vec::new();
 
-    // let mut rng = rand::thread_rng();
-    // for _i in 0..100 {
-    //     let x: f64 = rng.gen();
-    //     let y: f64 = rng.gen();
-    //     pts.push(vec![x, y, 2.0]);
-    // }
-
     pts.push(vec![1.1, 1.07, 12.5]);
     pts.push(vec![11.0, 1.02, 7.65]);
     pts.push(vec![11.05, 11.1, 33.0]);
     pts.push(vec![1.0, 11.0, 21.0]);
     pts.push(vec![9.0, 5.0, 21.0]);
     pts.push(vec![12.0, 5.1, 21.0]);
-    // pts.push(vec![8.0, 8.0, 21.0]);
-    // pts.push(vec![12.0, 8.1, 21.0]);
+    pts.push(vec![8.0, 8.0, 21.0]);
+    pts.push(vec![12.0, 8.1, 21.0]);
 
     pts.push(vec![4.0, 5.15, 33.0]);
 
@@ -32,21 +25,6 @@ fn main() {
         println!("!!!Error: {:?}", re.unwrap_err());
     }
     println!("{}", dt.printme());
-
-    // let re = dt.insert_one_pt(2.3, 2.3, 2.0);
-    // let re = dt.insert_one_pt(2.6, 2.6, 2.0);
-    // let re = dt.insert_one_pt(2.9, 2.6, 2.0);
-    // let re = dt.insert_one_pt(2.9, 2.1, 2.0);
-    // println!("{}", dt.printme());
-    // println!("{}", dt.printme());
-
-    // println!("{}", dt);
-    // dt.remove(6);
-    // println!("{}", dt.printme());
-
-    // let re = dt.insert_one_pt(3.3, 3.2, 2.0);
-    // let re = dt.insert_one_pt(2.3, 2.2, 22.0);
-    // println!("{}", dt.printme());
 
     println!("Is Delaunay?: {}", dt.is_valid());
     println!("# vertices {}", dt.number_of_vertices());
