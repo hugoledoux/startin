d6ef318e3a9c1c800a6661272d313609400f322a
Hugo Ledoux <h.ledoux@tudelft.nl>
2019-06-03 19:41:41 +0200

    Test cocircularity for delete

--- tests/deletion.rs
+++ tests/deletion.rs
@@ -21,6 +21,24 @@ fn simple() {
 }
 
 #[test]
+fn cocircular() {
+    let mut pts: Vec<Vec<f64>> = Vec::new();
+    pts.push(vec![0.0, 0.0, 12.5]);
+    pts.push(vec![1.0, 0.0, 7.65]);
+    pts.push(vec![1.0, 1.0, 33.0]);
+    pts.push(vec![0.0, 1.0, 21.0]);
+    let y: f64 = 0.5 + ((0.5 * 0.5 + 0.5 * 0.5) as f64).sqrt();
+    pts.push(vec![0.5, y, 21.0]);
+    pts.push(vec![0.5, 0.5, 33.0]);
+    let mut dt = startin::Triangulation::new();
+    dt.set_jump_and_walk(false);
+    dt.insert(&pts);
+    let _re = dt.remove(6);
+    assert_eq!(5, dt.number_of_vertices());
+    assert_eq!(3, dt.number_of_triangles());
+}
+
+#[test]
 fn deletion_impossible() {
     let mut pts: Vec<Vec<f64>> = Vec::new();
     pts.push(vec![1.1, 1.07, 12.5]);
