5f9168469d36a8cc82092d8fa46aa5ff5c591f1b
Hugo Ledoux <h.ledoux@tudelft.nl>
2019-06-04 08:29:35 +0200

    WIP tests

--- tests/deletion.rs
+++ tests/deletion.rs
@@ -20,6 +20,37 @@ fn simple() {
     assert_eq!(8, dt.number_of_triangles());
 }
 
+// #[test]
+// fn insert_delete() {
+//     let mut pts: Vec<Vec<f64>> = Vec::new();
+//     pts.push(vec![1.1, 1.07, 12.5]);
+//     pts.push(vec![11.0, 1.02, 7.65]);
+//     pts.push(vec![11.05, 11.1, 33.0]);
+//     let mut dt = startin::Triangulation::new();
+//     dt.set_jump_and_walk(false);
+//     dt.insert(&pts);
+//     let _re = dt.remove(3);
+//     assert_eq!(2, dt.number_of_vertices());
+//     assert_eq!(0, dt.number_of_triangles());
+// }
+
+#[test]
+fn collinear() {
+    let mut pts: Vec<Vec<f64>> = Vec::new();
+    pts.push(vec![0.0, 0.0, 12.5]);
+    pts.push(vec![1.0, 0.0, 7.65]);
+    pts.push(vec![2.0, 0.0, 33.0]);
+    pts.push(vec![3.0, 0.0, 33.0]);
+    pts.push(vec![4.0, 0.0, 33.0]);
+    let mut dt = startin::Triangulation::new();
+    dt.insert(&pts);
+    assert_eq!(5, dt.number_of_vertices());
+    assert_eq!(0, dt.number_of_triangles());
+    let _re = dt.insert_one_pt(3.0, 3.0, 33.0);
+    assert_eq!(6, dt.number_of_vertices());
+    assert_eq!(4, dt.number_of_triangles());
+}
+
 #[test]
 fn cocircular() {
     let mut pts: Vec<Vec<f64>> = Vec::new();
