a6c7eb432bdfcba4098598cf67f75864948fb69a
Hugo Ledoux <h.ledoux@tudelft.nl>
2019-07-29 11:38:27 +0200

    Fix warnings in example3.rs

--- examples/example3.rs
+++ examples/example3.rs
@@ -1,21 +1,19 @@
 extern crate startin;
 
 fn main() {
-    let mut pts: Vec<Vec<f64>> = Vec::new();
-
     let mut dt = startin::Triangulation::new();
     dt.set_jump_and_walk(false);
-    
-    dt.insert_one_pt(1.1, 1.07, 12.5);
-    dt.insert_one_pt(11.0, 1.02, 7.65);
-    dt.insert_one_pt(11.05, 11.1, 33.0);
-    dt.insert_one_pt(1.0, 11.0, 21.0);
+
+    let _re = dt.insert_one_pt(1.1, 1.07, 12.5);
+    let _re = dt.insert_one_pt(11.0, 1.02, 7.65);
+    let _re = dt.insert_one_pt(11.05, 11.1, 33.0);
+    let _re = dt.insert_one_pt(1.0, 11.0, 21.0);
 
     println!("{}", dt.printme(true));
     println!("Is Delaunay?: {}", dt.is_valid());
     println!("# vertices {}", dt.number_of_vertices());
     println!("# triangles {}", dt.number_of_triangles());
-    
+
     // let c = dt.closest_point(11.0, 11.0);
     // let re = dt.remove(c.unwrap());
     let re = dt.remove(3);
@@ -27,11 +25,13 @@ fn main() {
     println!("Is Delaunay?: {}", dt.is_valid());
     println!("# vertices {}", dt.number_of_vertices());
     println!("# triangles {}", dt.number_of_triangles());
-    
-    dt.insert_one_pt(11.0, 11.0, 22.2);
+
+    let _re = dt.insert_one_pt(11.0, 11.0, 22.2);
 
     println!("{}", dt.printme(true));
     println!("Is Delaunay?: {}", dt.is_valid());
     println!("# vertices {}", dt.number_of_vertices());
     println!("# triangles {}", dt.number_of_triangles());
+
+    println!("{:?}", dt.all_edges());
 }
