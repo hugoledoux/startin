f0d0e5de8cb27a30bc0ae0d75d7acde417690422
Hugo Ledoux <h.ledoux@tudelft.nl>
2021-03-08 10:50:55 +0100

    Keep both robust crate and my tweaked stolen code so that I can benchmark

--- src/geom/mod.rs
+++ src/geom/mod.rs
@@ -7,6 +7,8 @@
 
 mod exactpred;
 
+extern crate robust;
+
 pub fn det3x3t(a: &[f64], b: &[f64], c: &[f64]) -> f64 {
     ((a[0] - c[0]) * (b[1] - c[1])) - ((a[1] - c[1]) * (b[0] - c[0]))
 }
@@ -48,6 +50,7 @@ pub fn orient2d(a: &[f64], b: &[f64], c: &[f64], robust_predicates: bool) -> i8 
     //-- colinear = 0
     if robust_predicates == true {
         return orient2d_robust(&a, &b, &c);
+    // return orient2d_robust_new(&a, &b, &c);
     } else {
         return orient2d_fast(&a, &b, &c);
     }
@@ -57,11 +60,29 @@ pub fn orient2d_robust(a: &[f64], b: &[f64], c: &[f64]) -> i8 {
     //-- CCW    = +1
     //-- CW     = -1
     //-- colinear = 0
-    // let re = unsafe { shewchuk::orient2d(a.as_ptr(), b.as_ptr(), c.as_ptr()) };
+
     let re = exactpred::orient2d(a, b, c);
     if re == 0.0 {
         return 0;
     } else if re > 0.0 {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+
+pub fn orient2d_robust_new(a: &[f64], b: &[f64], c: &[f64]) -> i8 {
+    //-- CCW    = +1
+    //-- CW     = -1
+    //-- colinear = 0
+    let re = robust::orient2d(
+        robust::Coord { x: a[0], y: a[1] },
+        robust::Coord { x: b[0], y: b[1] },
+        robust::Coord { x: c[0], y: c[1] },
+    );
+    if re == 0.0_f64 {
+        return 0;
+    } else if re.is_sign_positive() {
         return 1;
     } else {
         return -1;
@@ -88,16 +109,16 @@ pub fn incircle(a: &[f64], b: &[f64], c: &[f64], p: &[f64], robust_predicates: b
     //-- p is ONCIRCLE == 0
     if robust_predicates == true {
         return incircle_robust(&a, &b, &c, &p);
+    // return incircle_robust_new(&a, &b, &c, &p);
     } else {
         return incircle_fast(&a, &b, &c, &p);
     }
 }
 
 pub fn incircle_robust(a: &[f64], b: &[f64], c: &[f64], p: &[f64]) -> i8 {
-    //-- CCW    = +1
-    //-- CW     = -1
-    //-- colinear = 0
-    // let re = unsafe { shewchuk::incircle(a.as_ptr(), b.as_ptr(), c.as_ptr(), p.as_ptr()) };
+    //-- p is INSIDE   == +1
+    //-- p is OUTSIDE  == -1
+    //-- p is ONCIRCLE == 0
     let re = exactpred::incircle(a, b, c, p);
     if re == 0.0 {
         return 0;
@@ -107,6 +128,26 @@ pub fn incircle_robust(a: &[f64], b: &[f64], c: &[f64], p: &[f64]) -> i8 {
         return -1;
     }
 }
+
+pub fn incircle_robust_new(a: &[f64], b: &[f64], c: &[f64], p: &[f64]) -> i8 {
+    //-- p is INSIDE   == +1
+    //-- p is OUTSIDE  == -1
+    //-- p is ONCIRCLE == 0
+    let re = robust::incircle(
+        robust::Coord { x: a[0], y: a[1] },
+        robust::Coord { x: b[0], y: b[1] },
+        robust::Coord { x: c[0], y: c[1] },
+        robust::Coord { x: p[0], y: p[1] },
+    );
+    if re == 0.0_f64 {
+        return 0;
+    } else if re.is_sign_positive() {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+
 pub fn incircle_fast(a: &[f64], b: &[f64], c: &[f64], p: &[f64]) -> i8 {
     //-- p is INSIDE   == +1
     //-- p is OUTSIDE  == -1
