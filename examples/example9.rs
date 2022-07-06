a72ae6c1ff97aae5bbfd832a435a59bbe4f7bf36
Hugo Ledoux <h.ledoux@tudelft.nl>
2022-07-06 08:15:48 +0200

    Clean the examples

--- examples/example9.rs
+++ examples/example9.rs
@@ -3,34 +3,45 @@ extern crate startin;
 fn main() {
     let mut pts: Vec<Vec<f64>> = Vec::new();
 
-    pts.push(vec![0.0, 0.0, 12.5]);
-    pts.push(vec![0.0, 0.0, 11.11]);
-    pts.push(vec![1.0, 0.0, 22.22]);
-    pts.push(vec![1.0, 1.0, 33.33]);
-    pts.push(vec![0.0, 1.0, 44.0]);
-    pts.push(vec![0.5, 0.49, 44.0]);
-    pts.push(vec![0.45, 0.69, 44.0]);
-    pts.push(vec![0.65, 0.49, 44.0]);
-    pts.push(vec![0.75, 0.29, 44.0]);
-    pts.push(vec![1.5, 1.49, 44.0]);
-    pts.push(vec![0.6, 0.2, 44.0]);
-    pts.push(vec![0.45, 0.4, 44.0]);
-    pts.push(vec![0.1, 0.8, 44.0]);
+    pts.push(vec![0.0, 0.0, 0.125]);
+    pts.push(vec![0.0, 0.0, 0.1111]);
+    pts.push(vec![1.0, 0.0, 0.2222]);
+    pts.push(vec![1.0, 1.0, 0.3333]);
+    pts.push(vec![0.0, 1.0, 0.440]);
+    pts.push(vec![0.5, 0.49, 0.440]);
+    pts.push(vec![0.45, 0.69, 0.440]);
+    pts.push(vec![0.65, 0.49, 0.440]);
+    pts.push(vec![0.75, 0.29, 0.440]);
+    pts.push(vec![1.5, 1.49, 0.440]);
+    pts.push(vec![0.6, 0.2, 0.440]);
+    pts.push(vec![0.45, 0.4, 0.440]);
+    pts.push(vec![0.1, 0.8, 0.440]);
 
     let mut dt = startin::Triangulation::new();
-    dt.insert(&pts);
+    // dt.insert(&pts, None);
+    dt.insert(&pts, Some(vec![-10.0, -10.0, 10.0, 10.0]));
 
     // let re = dt.interpolate_nn(2., 1.);
     // let re = dt.interpolate_nn(11., 11.);
     // let re = dt.interpolate_tin_linear(2., 1.);
-    let re = dt.interpolate_laplace(1., 1.);
-    println!("{:?}", re);
+    // let re = dt.interpolate_laplace(1., 1.);
+    // println!("{:?}", re);
 
-    let re = dt.interpolate_laplace(111., 111.);
-    println!("{:?}", re);
+    // let re = dt.interpolate_laplace(111., 111.);
+    // println!("{:?}", re);
 
     //-- some stats
     println!("Number of points in DT: {}", dt.number_of_vertices());
     println!("Number of triangles in DT: {}", dt.number_of_triangles());
     println!("bbox: {:?}", dt.get_bbox());
+
+    let _re = dt.write_geojson("/Users/hugo/temp/g1.geojson".to_string());
+
+    let pathout = "/Users/hugo/temp/out.obj";
+    println!("Writing OBJ file...");
+    let re = dt.write_obj(pathout.to_string(), false);
+    match re {
+        Ok(_x) => println!("--> OBJ output saved to: {}", pathout),
+        Err(_x) => println!("ERROR: path {} doesn't exist, abort.", pathout),
+    }
 }
--- examples/geotiff2tin.rs
+++ examples/geotiff2tin.rs
@@ -17,10 +17,6 @@ fn main() {
 
     let rasterband: RasterBand = dataset.rasterband(1).unwrap();
 
-    // println!("rasterband description: {:?}", rasterband.description());
-    println!("rasterband no_data_value: {:?}", rasterband.no_data_value());
-    // println!("rasterband type: {:?}", rasterband.band_type());
-    // println!("rasterband scale: {:?}", rasterband.scale());
     println!("rasterband offset: {:?}", rasterband.offset());
     println!("rasterband x-size: {:?}", rasterband.x_size());
     println!("rasterband y-size: {:?}", rasterband.y_size());
@@ -44,11 +40,8 @@ fn main() {
                     pts.push(vec![x, y, *z]);
                 }
             }
-            // println!("{:?}", rv.data);
-            // println!("{:?}", rv.data.len());
         }
     }
-    // println!("size: {:?}", pts.len());
 
     let mut dt = startin::Triangulation::new();
     dt.set_jump_and_walk(true);
@@ -64,7 +57,7 @@ fn main() {
     println!("Number of triangles in DT: {}", dt.number_of_triangles());
     println!("bbox: {:?}", dt.get_bbox());
 
-    let _re = dt.write_geojson("/Users/hugo/temp/g1.geojson".to_string());
+    // let _re = dt.write_geojson("/Users/hugo/temp/g1.geojson".to_string());
     // let pathout = "/Users/hugo/temp/out.obj";
     // println!("Writing OBJ file...");
     // let re = dt.write_obj(pathout.to_string(), false);
@@ -73,43 +66,3 @@ fn main() {
     //     Err(_x) => println!("ERROR: path {} doesn't exist, abort.", pathout),
     // }
 }
-
-// use las::{Read, Reader};
-
-// fn main() {
-//     let path = std::env::args()
-//         .skip(1)
-//         .next()
-//         .expect("Must provide a path to a LAS/LAZ file");
-//     let mut reader = Reader::from_path(path).expect("Wrong file name");
-
-//     let header = reader.header();
-//     println!("Reading LAS file version: {}", header.version());
-//     println!("{} points.", header.number_of_points());
-
-//     // let b = header.bounds();
-//     // println!(
-//     // "({}, {}, {}) --> ({}, {}, {})",
-//     // b.min.x, b.min.y, b.min.z, b.max.x, b.max.y, b.max.z
-//     // );
-
-//     let mut dt = startin::Triangulation::new();
-//     dt.set_jump_and_walk(true);
-
-//     for laspt in reader.points() {
-//         let p = laspt.unwrap();
-//         let _re = dt.insert_one_pt(p.x, p.y, p.z);
-//     }
-
-//     println!("Number of points in DT: {}", dt.number_of_vertices());
-//     println!("Number of triangles in DT: {}", dt.number_of_triangles());
-//     println!("bbox: {:?}", dt.get_bbox());
-
-//     // let pathout = "/Users/hugo/temp/out.obj";
-//     // println!("Writing OBJ file...");
-//     // let re = dt.write_obj(pathout.to_string(), false);
-//     // match re {
-//     //     Ok(_x) => println!("--> OBJ output saved to: {}", pathout),
-//     //     Err(_x) => println!("ERROR: path {} doesn't exist, abort.", pathout),
-//     // }
-// }
