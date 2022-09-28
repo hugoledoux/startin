7400d3e86442e51aae37eec44c186f3e24588528
Hugo Ledoux <h.ledoux@tudelft.nl>
2022-09-28 15:14:39 +0200

    Update examples to use the collect_garbage()

--- examples/laz2tin.rs
+++ examples/laz2tin.rs
@@ -2,6 +2,7 @@ extern crate las;
 extern crate startin;
 
 use las::{Read, Reader};
+use rand::{thread_rng, Rng};
 
 fn main() {
     let path = std::env::args()
@@ -21,22 +22,37 @@ fn main() {
     // );
 
     let mut dt = startin::Triangulation::new();
-    dt.set_jump_and_walk(true);
+    // dt.set_jump_and_walk(true);
 
+    let mut rng = thread_rng();
+    let thin_factor = 100;
     for laspt in reader.points() {
-        let p = laspt.unwrap();
-        let _re = dt.insert_one_pt(p.x, p.y, p.z);
+        if rng.gen_ratio(1, thin_factor) == true {
+            let p = laspt.unwrap();
+            let _re = dt.insert_one_pt(p.x, p.y, p.z);
+        }
     }
 
     println!("Number of points in DT: {}", dt.number_of_vertices());
     println!("Number of triangles in DT: {}", dt.number_of_triangles());
     println!("bbox: {:?}", dt.get_bbox());
 
-    // let pathout = "/Users/hugo/temp/out.obj";
-    // println!("Writing OBJ file...");
-    // let re = dt.write_obj(pathout.to_string(), false);
+    for i in 100..150 {
+        // println!("{}", i);
+        let _re = dt.remove(i);
+    }
+
+    dt.collect_garbage();
+    assert_eq!(dt.is_valid(), true);
+    assert_eq!(dt.has_garbage(), false);
+
+    println!("=={}", dt.number_of_removed_vertices());
+    // dt.vertical_exaggeration(2.0);
+    // let pathout = "/Users/hugo/temp/t1.ply";
+    // println!("Writing PLY file...");
+    // let re = dt.write_ply(pathout.to_string());
     // match re {
-    //     Ok(_x) => println!("--> OBJ output saved to: {}", pathout),
+    //     Ok(_x) => println!("--> PLY output saved to: {}", pathout),
     //     Err(_x) => println!("ERROR: path {} doesn't exist, abort.", pathout),
     // }
 }
