c2abd5ba4f8cb75ebb616b3fe7170d3869efaadb
Hugo Ledoux <h.ledoux@tudelft.nl>
2023-01-05 11:11:09 +0100

    Add example file to compare times with storing Voronoi areas

--- examples/example6.rs
+++ examples/example6.rs
@@ -1,37 +1,117 @@
+extern crate las;
 extern crate startin;
 
-fn main() {
-    let mut pts: Vec<[f64; 3]> = Vec::new();
+use las::{Read, Reader};
+use rand::distributions::{Distribution, Uniform};
+use rand::{thread_rng, Rng};
+use std::time::Instant;
 
-    pts.push([57.42265069953759, 11.302054173605036, 2.0]);
-    pts.push([92.84366030693992, 43.6916136057666, 2.0]);
-    pts.push([1.4236330964329302, 64.90700146602387, 2.0]);
-    pts.push([13.285710267579498, 50.189815660581175, 2.0]);
-    pts.push([15.111123388743696, 78.78533829573448, 2.0]);
+fn main() {
+    let path = std::env::args()
+        .skip(1)
+        .next()
+        .expect("Must provide a path to a LAS/LAZ file");
+    let mut reader = Reader::from_path(path).expect("Wrong file name");
+
+    let header = reader.header();
+    println!("Reading LAS file version: {}", header.version());
+    println!("{} points.", header.number_of_points());
+
+    let b = header.bounds();
+    println!(
+        "({}, {}, {}) --> ({}, {}, {})",
+        b.min.x, b.min.y, b.min.z, b.max.x, b.max.y, b.max.z
+    );
 
     let mut dt = startin::Triangulation::new();
-    dt.insert(&pts, startin::InsertionStrategy::BBox);
-    println!("{}", dt);
 
-    for (i, each) in dt.all_vertices().iter().enumerate() {
-        println!("#{}: ({:.3}, {:.3}, {:.3})", i, each[0], each[1], each[2]);
+    let mut rng = thread_rng();
+    let thin_factor = 1;
+    for laspt in reader.points() {
+        if rng.gen_ratio(1, thin_factor) == true {
+            let p = laspt.unwrap();
+            let _re = dt.insert_one_pt(p.x, p.y, p.z);
+        }
     }
 
-    for (i, each) in dt.all_triangles().iter().enumerate() {
-        println!("#{}: ({})", i, each);
+    println!("Number of points in DT: {}", dt.number_of_vertices());
+    println!("Number of triangles in DT: {}", dt.number_of_triangles());
+    println!("bbox: {:?}", dt.get_bbox());
+
+    //-- interpolations
+
+    let rx = Uniform::from(b.min.x..b.max.x);
+    let ry = Uniform::from(b.min.y..b.max.y);
+    let mut locs = Vec::new();
+    for _ in 0..100000 {
+        locs.push([rx.sample(&mut rng), ry.sample(&mut rng)]);
     }
 
-    println!("no removed: {}", dt.number_of_removed_vertices());
+    {
+        let now = Instant::now();
+        {
+            for loc in &locs {
+                let re = dt.interpolate_nni(loc[0], loc[1]);
+            }
+        }
+        let elapsed = now.elapsed();
+        println!("nni: {:.2?}", elapsed);
+    }
+    {
+        let now = Instant::now();
+        {
+            let re = dt.interpolate_nni_2(&locs);
+        }
+        let elapsed = now.elapsed();
+        println!("nni_2: {:.2?}", elapsed);
+    }
 
-    // let _re = dt.write_obj("/Users/hugo/temp/0.obj".to_string(), true);
+    {
+        let now = Instant::now();
+        {
+            for loc in &locs {
+                let re = dt.interpolate_laplace(loc[0], loc[1]);
+            }
+        }
+        let elapsed = now.elapsed();
+        println!("laplace: {:.2?}", elapsed);
+    }
+    {
+        let now = Instant::now();
+        {
+            let re = dt.interpolate_tin_linear_2(&locs);
+        }
+        let elapsed = now.elapsed();
+        println!("tin_linear_2: {:.2?}", elapsed);
+    }
 
-    // let _re = dt.remove(11);
-    // println!("{}", dt.printme(true));
+    // println!("{:?}", re);
 
-    // assert!(dt.is_valid());
-    // let _re = dt.write_obj("/Users/hugo/temp/1.obj".to_string(), true);
+    //-- interpolate
+    // let locs = vec![[51.0, 42.0]];
+    // let re = dt.interpolate_nn_2(&locs);
+    // let re = dt.interpolate_nn_2(&vec![[51.0, 42.0]]);
+    // for each in &re {
+    //     match each {
+    //         Ok(z) => println!("z = {}", z),
+    //         Err(why) => println!("Interplation impossible: {:?}", why),
+    //     }
+    // }
 
-    // let _re = dt.insert_one_pt(1.5, 1.5, 33.0);
-    // println!("{}", dt.printme(true));
-    // assert!(dt.is_valid());
+    // let re = dt.interpolate_tin_linear_2(&vec![[51.0, 52.0]]);
+    // for each in &re {
+    //     match each {
+    //         Ok(z) => println!("z = {}", z),
+    //         Err(why) => println!("Interplation impossible: {:?}", why),
+    //     }
+    // }
+
+    // let a = dt.interpolate_laplace(51.0, 48.0);
+    // println!("{:?}", a);
+    // let b = dt.interpolate_laplace_2(&vec![[51.0, 48.0]]);
+    // println!("{:?}", b);
+
+    //-- save the triangulation in geojson for debug purposes
+    //-- do not attempt on large DT
+    // let _re = dt.write_geojson("/home/elvis/tr.geojson".to_string());
 }
