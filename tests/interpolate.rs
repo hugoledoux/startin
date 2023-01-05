0b95b183bebfc7dbc13547dcea4208dbb8f06f9b
Hugo Ledoux <h.ledoux@tudelft.nl>
2023-01-05 15:07:03 +0100

    Fix unit tests + examples for the new API (temp, maybe it'll change)

--- tests/interpolate.rs
+++ tests/interpolate.rs
@@ -30,19 +30,19 @@ fn empty() {
     let mut dt = startin::Triangulation::new();
     assert_eq!(
         Err(startin::StartinError::NoTriangleinTIN),
-        dt.interpolate_nn(1.1, 1.1)
+        dt.interpolate_nn(&vec![[51.0, 42.0]])[0]
     );
     assert_eq!(
         Err(startin::StartinError::NoTriangleinTIN),
-        dt.interpolate_nni(1.1, 1.1)
+        dt.interpolate_nni(&vec![[51.0, 42.0]], true)[0]
     );
     assert_eq!(
         Err(startin::StartinError::NoTriangleinTIN),
-        dt.interpolate_laplace(1.1, 1.1)
+        dt.interpolate_laplace(&vec![[51.0, 42.0]])[0]
     );
     assert_eq!(
         Err(startin::StartinError::NoTriangleinTIN),
-        dt.interpolate_tin_linear(1.1, 1.1)
+        dt.interpolate_tin_linear(&vec![[51.0, 42.0]])[0]
     );
 }
 
@@ -51,23 +51,23 @@ fn outside_ch() {
     let mut dt = four_points();
     assert_eq!(
         Err(startin::StartinError::OutsideConvexHull),
-        dt.interpolate_nn(5.0, -0.1)
+        dt.interpolate_nn(&vec![[5.0, -0.1]])[0]
     );
     assert_eq!(
         Err(startin::StartinError::OutsideConvexHull),
-        dt.interpolate_nni(5.0, -0.1)
+        dt.interpolate_nni(&vec![[5.0, -0.1]], false)[0]
     );
     assert_eq!(
         Err(startin::StartinError::OutsideConvexHull),
-        dt.interpolate_laplace(5.0, -0.1)
+        dt.interpolate_laplace(&vec![[5.0, -0.1]])[0]
     );
     assert_eq!(
         Err(startin::StartinError::OutsideConvexHull),
-        dt.interpolate_tin_linear(5.0, -0.1)
+        dt.interpolate_tin_linear(&vec![[5.0, -0.1]])[0]
     );
     assert_eq!(
         Err(startin::StartinError::OutsideConvexHull),
-        dt.interpolate_nni(5.0, 0.0)
+        dt.interpolate_nni(&vec![[5.0, 0.0]], true)[0]
     );
 }
 
@@ -75,34 +75,34 @@ fn outside_ch() {
 fn existing_point() {
     let mut dt = four_points();
     let _re = dt.insert_one_pt(5.0, 5.0, 11.1);
-    assert_eq!(Ok(11.1), dt.interpolate_nn(5.0, 5.0));
-    assert_eq!(Ok(11.1), dt.interpolate_nni(5.0, 5.0));
-    assert_eq!(Ok(11.1), dt.interpolate_laplace(5.0, 5.0));
-    assert_eq!(Ok(11.1), dt.interpolate_tin_linear(5.0, 5.0));
-    assert_eq!(Ok(1.0), dt.interpolate_nn(0.0, 0.0));
-    assert_eq!(Ok(1.0), dt.interpolate_nni(0.0, 0.0));
-    assert_eq!(Ok(1.0), dt.interpolate_laplace(0.0, 0.0));
-    assert_eq!(Ok(1.0), dt.interpolate_tin_linear(0.0, 0.0));
+    assert_eq!(Ok(11.1), dt.interpolate_nn(&vec![[5.0, 5.0]])[0]);
+    assert_eq!(Ok(11.1), dt.interpolate_nni(&vec![[5.0, 5.0]], false)[0]);
+    assert_eq!(Ok(11.1), dt.interpolate_laplace(&vec![[5.0, 5.0]])[0]);
+    assert_eq!(Ok(11.1), dt.interpolate_tin_linear(&vec![[5.0, 5.0]])[0]);
+    assert_eq!(Ok(1.0), dt.interpolate_nn(&vec![[0.0, 0.0]])[0]);
+    assert_eq!(Ok(1.0), dt.interpolate_nni(&vec![[0.0, 0.0]], false)[0]);
+    assert_eq!(Ok(1.0), dt.interpolate_laplace(&vec![[0.0, 0.0]])[0]);
+    assert_eq!(Ok(1.0), dt.interpolate_tin_linear(&vec![[0.0, 0.0]])[0]);
 }
 
 #[test]
 fn middle() {
     let mut dt = four_points();
-    assert_eq!(Ok(2.5), dt.interpolate_nni(5.0, 5.0));
-    assert_eq!(Ok(2.5), dt.interpolate_laplace(5.0, 5.0));
+    assert_eq!(Ok(2.5), dt.interpolate_nni(&vec![[5.0, 5.0]], true)[0]);
+    assert_eq!(Ok(2.5), dt.interpolate_laplace(&vec![[5.0, 5.0]])[0]);
 }
 
 #[test]
 fn nn() {
     let mut dt = four_points();
     let _re = dt.insert_one_pt(5.0, 5.0, 11.1);
-    assert_eq!(Ok(11.1), dt.interpolate_nn(5.1, 5.1));
+    assert_eq!(Ok(11.1), dt.interpolate_nn(&vec![[5.1, 5.1]])[0]);
 }
 
 #[test]
 fn tin_linear() {
     let dt = four_points();
-    assert_eq!(Ok(1.5), dt.interpolate_tin_linear(5.0, 0.0));
+    assert_eq!(Ok(1.5), dt.interpolate_tin_linear(&vec![[5.0, 0.0]])[0]);
 }
 
 #[test]
@@ -110,14 +110,17 @@ fn tin_linear_random() {
     let dt = random_points_500();
     assert_eq!(
         Err(startin::StartinError::OutsideConvexHull),
-        dt.interpolate_tin_linear(144.0, 48.0)
+        dt.interpolate_tin_linear(&vec![[144.0, 48.0]])[0]
     );
-    assert_eq!(true, dt.interpolate_tin_linear(44.0, 48.0).is_ok());
+    assert_eq!(
+        true,
+        dt.interpolate_tin_linear(&vec![[44.0, 48.0]])[0].is_ok()
+    );
 }
 
 #[test]
 fn nni_boundary_ch() {
     let mut dt = four_points();
-    let re = dt.interpolate_nni(10.0, 5.0);
-    assert_eq!(false, re.is_ok());
+    let re = dt.interpolate_nni(&vec![[10.0, 5.0]], false);
+    assert_eq!(false, re[0].is_ok());
 }
